2eb68d0af051885393d9777f5103284ab715afff
Coding <m64513824@gmail.com>
2025-08-15 01:31:46 +0300

    Creating the user and course functions

--- headers/course.h
+++ headers/course.h
@@ -1,6 +1,36 @@
 #ifndef COURSES_H_INCLUDED
 #define COURSES_H_INCLUDED
 
+#include <iostream>
+#include <vector>
 
+class Student;
+class Assignment;
+
+class Course {
+    private:
+        int id;
+        char coursePrefix = 'c';
+        std::string name;
+        std::string docID;
+        std::vector<std::string> studentsIDs;
+        std::vector<std::string> assignmentsIDs;
+        std::string filePath = "../data/courses.csv";
+
+    public:
+        Course(): id(0) {}
+        Course(int id) { this->setID(id); }
+
+        void setID(int id);
+
+        static std::string addCourse(std::string name, std::string docID);
+
+        std::string getName() const;
+        std::string getID() const;
+        std::vector<Student*> getStudetns() const;
+        std::vector<Assignment*> getAssignments() const;
+        std::string getDoctor();
+        void addStudent(std::string stuID);
+};
 
 #endif // COURSES_H_INCLUDED
---
+++ src/user.cpp
@@ -0,0 +1,32 @@
+#include "../headers/user.h"
+#include "../headers/functions.h"
+#include "../headers/course.h"
+
+std::string User::getID() const {
+    return (this->userPrefix + std::to_string(this->id));
+}
+std::string User::getName() const {
+    return this->name;
+}
+std::string User::getUsername() const {
+    return this->username;
+}
+std::vector<Course*> User::getCourses() const {
+    if (this->coursesIDs.empty()) return {};
+    if (this->coursesIDs.size() == 1) {
+        if (this->coursesIDs.at(0).empty()) {
+            return {};
+        }
+    }
+    std::vector<Course*> courses;
+    for (std::string s: this->coursesIDs) {
+        int id = stringToInt(s);
+        Course* c = new Course(id);
+        courses.push_back(c);
+    }
+    return courses;
+}
+void User::setID(std::string id) {
+    this->id = 0;
+    this->userPrefix = 'p';
+}
