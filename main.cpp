c7fdb646913988b80eadd3c6d201428e3835de6d
Coding <m64513824@gmail.com>
2025-08-15 00:16:58 +0300

    Update the main file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "headers/functions.h"
 using namespace std;
 
 int main()
 {
-    
+    cout<<"================================================\n";
+    cout<<"==  Welcome To Educational Management System  ==\n";
+    cout<<"================================================\n";
+    welcome();
+
     return 0;
 }
