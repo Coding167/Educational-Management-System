b99b1f71c4f8ca7ce9145cd7b22174b5731df2c7
Coding <m64513824@gmail.com>
2025-08-17 11:38:36 +0300

    Adding welcome message

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "../headers/functions.h"
 
 int main() {
+    // Welcome message
+    std::cout<<"\tWelcome to the Educational Management System!\n\n";
+
+    // Start the application
     welcome();
+    
     return 0;
 }
